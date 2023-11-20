6bf9d51d50826f92b4e33b73356660719b21c67f
Bipul chhetri <81376867+bipulchhetri@users.noreply.github.com>
2023-11-20 15:10:21 +0000

    datatypes

---
+++ Basic/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat js code as newr version
+//alert("hello") // we are using node js not browser
+let age=25;
+//Datatypes
+/*
+int 
+bigint
+string
+boolean
+null=standalone value
+undefined=>
+symbol
+
+
+objects
+*/ 
+console.log( typeof null); // object
+console.log( typeof undefined);
