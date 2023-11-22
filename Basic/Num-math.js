1254e0c1bca1be82901fbe80c290edf088c8870a
Bipul chhetri <81376867+bipulchhetri@users.noreply.github.com>
2023-11-22 15:50:49 +0000

    Update

---
+++ Basic/Num-math.js
@@ -0,0 +1,15 @@
+const num=10000;
+console.log(num.toLocaleString())
+
+const price=1235.00;
+console.log(price.toPrecision(4))
+
+// Math****
+
+console.log(Math) //objects types
+console.log(Math.abs(-10))
+console.log(Math.round(4.5))
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
