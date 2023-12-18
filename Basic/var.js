79df597e5905fdea29438510a35c1d6da158e87d
Bipul chhetri <81376867+bipulchhetri@users.noreply.github.com>
2023-12-18 17:13:00 +0000

    Updated

--- Basic/var.js
+++ Basic/var.js
@@ -3,11 +3,37 @@ let accountEmail="bipul@google.com";
 var city="not found"
 
 accountEmail="hello@gmail.com"
-accountId=687687;
-console.table([accountId,accountEmail,city])
+// accountId=687687;
+// console.table([accountId,accountEmail,city])
 
 
 /*
 Not use var
-Because of issues of Block scope and function socpe
+Because of issues of Block scope and functional socpe
 */
+
+const AccountId=564;
+let AccountEmail="bipulgoogle@gmail.com"
+var AccountCity="Bangalore"
+// AccountId=65;
+AccountEmail="sdg"
+password="454"
+console.log(AccountId)
+console.log(AccountEmail)
+
+// In Js there are Global and local scope 
+//using Below example you can understand let const var clearly
+if (true)
+{
+    let a=10;
+    const b=30;
+    var c=50;
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+// var within the local scope still you can access here this the main issue in js 
+// so nowdays we dont use var 
+
+// ** More scope explain in Scope folder 
